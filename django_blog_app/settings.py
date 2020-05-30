0a61953c69ac326ca858899d76fc795f0f85eda0
mgunes <mistafagunes@gmail.com>
2020-05-30 20:01:20 +0300

    added crispy

--- django_blog_app/settings.py
+++ django_blog_app/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
